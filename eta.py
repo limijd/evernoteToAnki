9e0f643a71fed0c7d4ed11aff1567d61603431b3
Wei Li <limijd@protonmail.com>
2020-10-21 20:55:55 -0700

    basic functionality to fetch notes metadata from a notebook works now

--- eta.py
+++ eta.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+#API reference: https://dev.evernote.com/doc/reference/NoteStore.html
+
 import os
 import sys
 import json
@@ -12,23 +14,56 @@ import hashlib
 import binascii
 import evernote.edam.userstore.constants as UserStoreConstants
 import evernote.edam.type.ttypes as Types
+import evernote.edam.notestore.ttypes as ns_ttypes
 from evernote.api.client import EvernoteClient
 
-token_file = os.path.expanduser("~/.evernote_token.json")
-token = None
+class NotebookReader:
+    def __init__(self, notebook_name):
+        self.notebook_name = notebook_name
+        self.token = self.readToken()
+        assert self.token
 
-with open(token_file, "r") as fp:
-    js = json.load(fp)
-    token = js["token"]
+        self.ev_client = None
+        self.ev_note_store = None
+        self.ev_notebook = None
+        self.ev_notes_metadata = []
+        self.ev_notes = {}
 
-assert token
+        self.clientConnect()
+        self.fetchNotesMetadata()
+        return
+
+    def readToken(self):
+        token_file = os.path.expanduser("~/.evernote_token.json")
+        with open(token_file, "r") as fp:
+            js = json.load(fp)
+            assert "token" in js
+            token = js["token"]
+            return token
+        return None
 
 
-#sandbox=False means use production service with Developer token
-client = EvernoteClient(token=token, sandbox=False, china=False)
+    def clientConnect(self):
+        self.ev_client = EvernoteClient(token=self.token, sandbox=False, china=False)
+        self.ev_note_store = self.ev_client.get_note_store()
+        notebooks = self.ev_note_store.listNotebooks()
+        for nb in notebooks:
+            if nb.name == self.notebook_name:
+                self.ev_notebook = nb
 
-note_store = client.get_note_store()
 
-notebooks = note_store.listNotebooks()
-for nb in notebooks:
-    print "notebook: ", nb.name
+    def fetchNotesMetadata(self):
+        flt = ns_ttypes.NoteFilter()
+        flt.notebookGuid = self.ev_notebook.guid
+        rs = ns_ttypes.NotesMetadataResultSpec()
+        rs.includeTitle = True
+        nml = self.ev_note_store.findNotesMetadata(self.token, flt, 0, 10000, rs)
+        for n in nml.notes:
+            self.ev_notes_metadata.append([n.guid, n.title])
+
+        return
+
+if __name__ == "__main__":
+    nr = NotebookReader("AnkiQuickNotes")
+    for n in nr.ev_notes_metadata:
+        print n[0], n[1]
